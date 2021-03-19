a87b14853cded2cd1e1a620d7e54ef6d79461a7d
Justin <walkerj90@icloud.com>
2021-03-19 00:52:55 -0700

    added models to build on

--- models/index.js
+++ models/index.js
@@ -5,13 +5,21 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
-
+Product.belongsTo(Category, {
+  foreignKey: 'category_id',
+});
 // Categories have many Products
-
+Category.hasMany(Product, {
+  foreignKey: 'category_id',
+});
 // Products belongToMany Tags (through ProductTag)
-
+Product.belongsToMany(Tags, {
+  foreignKey: 'product_id',
+});
 // Tags belongToMany Products (through ProductTag)
-
+Tags.belongsToMany(Product, {
+  foreignKey: 'tag_id',
+});
 module.exports = {
   Product,
   Category,
