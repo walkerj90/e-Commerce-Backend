fa9c530172f078f53a84ad95bc3abca2e83e47fd
Justin Walker <walkerj90@icloud.com>
2021-03-25 02:34:13 -0700

    finished tag routes

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -52,10 +52,45 @@ router.post('/', (req, res) => {
 
 router.put('/:id', (req, res) => {
   // update a tag's name by its `id` value
+  Tag.update({
+    tag_name: req.body.tag_name
+  },
+  {
+    where: {
+      id: req.params.id
+    }
+  })
+.then (tagData => {
+  if (!tagData){
+    res.status(404).json({ message: 'No tag found with that ID.'});
+    return;
+  }
+  res.json(tagData);
+})
+.catch(err=> {
+  console.log(err);
+  res.status(500).json(err);
+});
 });
 
 router.delete('/:id', (req, res) => {
   // delete on tag by its `id` value
+  Tag.destroy({
+    where: {
+      id: req.params.id
+    }
+  })
+    .then(tagData => {
+      if (!tagData) {
+        res.status(404).json({ message: 'No Tag found by that ID.' });
+        return;
+      }
+      res.json(tagData);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 module.exports = router;
